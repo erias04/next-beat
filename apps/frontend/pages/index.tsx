e532644e219ce85ee8f5fcb27cdddd1911ec9572
Elias Cerne <eliascerne@icloud.com>
2023-03-02 19:16:16 +0100

    feat: add text to / page

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome frontend 👋
+              Welcome frontend, welcome 👋
             </h1>
           </div>
 
